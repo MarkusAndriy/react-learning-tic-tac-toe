8165da918ff3ab2f4e7a6abbe9a2e430f5d75ac7
andriymarkus1 <mak270106@gmail.com>
2025-04-19 18:23:55 +0300

    Introduce basic interactive Tic-Tac-Toe components
    Added `Player`, `GameBoard`, and main game logic to enable basic Tic-Tac-Toe gameplay. Players can take turns, edit names, and click squares to update the game board.

---
+++ src/components/Player.jsx
@@ -0,0 +1,30 @@
+﻿import { useState } from 'react';
+
+export default function Player({initialName, symbol, isActive}) {
+
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleBtnToggle() {
+    setIsEditing(prev => !prev);
+  }
+  function handleInputChange(e) {
+    setPlayerName(e.target.value);
+  }
+
+  let playerNameElement = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    playerNameElement = <input type="text" value={playerName} required onChange={handleInputChange}/>;
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {playerNameElement}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleBtnToggle}>{isEditing ? "Save" : "Edit" }</button>
+    </li>
+  )
+}
